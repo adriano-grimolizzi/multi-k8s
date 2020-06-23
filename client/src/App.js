0f7fe97ed69063cc5e5d39f39465f1ea2fa64d25
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2020-06-23 12:28:36 +0200

    change app title to ODIO TIBCO

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version Adriano!</h1>
+            <h1 className="App-title">ODIO TIBCO</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
